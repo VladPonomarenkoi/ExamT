cc41404536082cef934b8438ceca8cd3add9f15f
Vlad Ponomarenko <gggrrry392@gmail.com>
2022-12-19 11:24:17 +0200

    add console interaction~

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const readline = require('readline-sync');
 const arithmeticProgression = (initial, step) => (n) => {
     let resultProgression = [];
     let nextValue = initial;
@@ -12,9 +13,14 @@ const arithmeticProgression = (initial, step) => (n) => {
 
 const getTaskArithmeticProgression = arithmeticProgression(1, 2); // start value = 1, step = 2
 
+console.log('Пономаренко Владислав');
 console.log('Арифметична прогресія з n=2 (1+3)', getTaskArithmeticProgression(2));
 console.log('Арифметична прогресія з n=4 (1+3+5+7)', getTaskArithmeticProgression(4));
 console.log('Арифметична прогресія з n=6 (1+3+5+7+9+11)', getTaskArithmeticProgression(6));
 console.log('Арифметична прогресія з n=8 (1+3+5+7+9+11+13+15)', getTaskArithmeticProgression(8));
+
+const consoleN = readline.question('Введіть N для арифметичної прогресії: ')
+console.log(`Арифметична прогресія з n=${consoleN} ${getTaskArithmeticProgression(Number(consoleN))}`);
+
 
 module.exports = getTaskArithmeticProgression
